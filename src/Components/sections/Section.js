05d2f3e89a3b59d14507fe74033431877cf5cb76
Mohammad Asif Siddique <mdasifsidd@gmail.com>
2022-03-13 10:36:37 +0530

    first commit

--- src/Components/sections/Section.js
+++ src/Components/sections/Section.js
@@ -1,20 +1,27 @@
 import React,{useState} from 'react'
 import SingleSection from '../single-section/SingleSection'
 import './Section.css';
-import video1 from './video-1.mp4';
+
+
+
+let initialPath = "./videos/video-5.mp4" ;
 
 const Section = ({ list }) => {
-    const [path,setpath]= useState("")
-    return (
+    const [path,setPath]= useState("./videos/video-1.mp4");
+    console.log("line 9 " + path);
+        return (
         <div className='section'>
-            <video>
-                <source src={video1}></source>
+            <video  controls muted autoPlay loop className='videoSection'>
+                <source src={require(`${path}`)} type="video/mp4"></source>
             </video>
+            {
+                console.log("im here")
+            }
             <div className='content'>
 
             {
                 list.map((item) => {
-                    return <SingleSection prop={path} key={item.id} item={item}/>
+                    return <SingleSection setPath={setPath} key={item.id} item={item}/>
                 })
             }
             </div>
--- src/Components/single-section/SingleSection.js
+++ src/Components/single-section/SingleSection.js
@@ -5,23 +5,28 @@ import './SingleSection.css'
 
 
 
-const SingleSection = ({ item }) => {
+const SingleSection = ({ item, setPath }) => {
   const { id, title, subtitle, items } = item
   const playVideo = (e) => {
     // e.target.play()
-    console.log(e.target);
-    console.log("On");
+    // console.log(e.target);
+    // console.log("On");
   }
   const pauseVideo = (e) => {
     // e.target.stop()
     // console.log(e.target);
     // console.log("Off");
   }
-
+   function PlayVideo(path){
+     
+    setPath(path);
+    // console.log(path);
+    // video.play();
+   }
 
 
   return (
-    <div className='section-container' onMouseEnter={playVideo} >
+    <div className='section-container' onMouseEnter={()=>PlayVideo(item.video)} >
       {/* <video controls autoPlay width="100%" height="100%">
         <source src = {require(`${item.video}`)} type='video/mp4' ></source>
   
